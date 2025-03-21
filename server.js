1ec30ee522e60791327106fcaf1e625a8e2997ec
stephany-c <ster.carolinas@hotmail.com>
2025-03-20 23:22:26 -0300

    correção de server.js

--- server.js
+++ server.js
@@ -4,42 +4,59 @@ import cors from 'cors';
 
 const prisma = new PrismaClient()
 
-const app = express ()
+const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post('/usuarios', async (req , res)=>{
+app.post('/usuarios', async (req, res) => {
     await prisma.user.create({
         data: {
-          email: req.body.email,
-          name: req.body.name,
-          age: req.body.age
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
         }
-      });
- res.status(201).json(req.body)
+    });
+    res.status(201).json(req.body)
 })
 
-app.get('/usuarios', async (req , res)=>{
+app.get('/usuarios', async (req, res) => {
+    let users = [];
 
-  const users = await prisma.user.findMany()
+    if (req.query.name) {
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name
+            }
+        });
+    } else {
+        users = await prisma.user.findMany();
+    }
 
-    res.status(200).json(users)
+    res.status(200).json(users);
+});
+
+app.put('/usuarios/:id', async (req, res) => {
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    });
+    res.status(201).json(req.body)
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id,
+        },
+    });
+
+    res.status(200).json({ message: "Usuário deletado com sucesso!" })
 })
 
 app.listen(3000)
-
-
-/*
--Criar um usuário
--Listar todos os usuários
--Editar um usuários
--deletar um usuários
-*/
-/* 1) Tipo de rota / Métodos HTTP
-   2) Endereço
-
-Nome de usuário: tethcarol
-Senha: gXEyUoAUClJq06yG
-*/
-
- 
